df10af13bc377acad3df2cf58f787009729e0234
David McCaffrey <dmccaffrey@vmware.com>
2023-11-17 15:22:46 -0600

    Setup fixes

--- shell/setup.sh
+++ shell/setup.sh
@@ -1,7 +1,7 @@
 shell_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "
 # git@github.com:dmccaffrey/devtools.git
-for f in ${shell_dir}/*_functions.sh; do source $f; done
+for f in ${shell_dir}/*_functions.sh; do source \$f; done
 enable_git_functions
 enable_json_functions
 # git@github.com:dmccaffrey/devtools.git
